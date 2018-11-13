28c4555a955cf13aab603e5c2ff5bbfc20aef7e5
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 16:35:14 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,23 +11,24 @@ def third_challenge
         },
       :hero => {
         :name: "Romeo",
-        :age: "15"
+        :age: "15",
         :status: "alive" 
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+        :name: "Lord Capulet",
+        :age: "50"
         },
       :matriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+        :name: "Lady Capulet",
+        :age: "51"
         },
       :heroine => {
-        :name => "Lord Montague",
-        :age => "53"
+        :name: "Juliet",
+        :age: "15",
+        :status: "alive"
         },
       :heroine_friends => []
    }
