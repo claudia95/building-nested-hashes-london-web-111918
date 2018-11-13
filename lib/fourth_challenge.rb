296cc38a4ec2da0dc915f5ca496599861eac0404
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 16:41:44 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -43,6 +43,11 @@ def fourth_challenge
         :name: "Steven",
         :age: "30",
         :attitude: "confused"
+      },
+      {
+        :name: "Nurse",
+        :age: "44",
+        :attitude: "worried"
       }]
    }
 }
